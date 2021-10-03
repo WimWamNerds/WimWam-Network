3926f659e4ed11eef1175d9a19dc2858d4774f7e
WimWamNerds <chrisisnerdy@gmail.com>
2021-10-02 20:23:08 -0700

    WimWam

--- public/js/src/f&d/directives.js
+++
@@ -1,9 +0,0 @@
-// DIRECTIVES
-
-import Vue from 'vue'
-
-// Another way to change page's title
-Vue.directive('title', {
-  inserted: (el, binding) => document.title = binding.value,
-  update: (el, binding) => document.title = binding.value
-})
