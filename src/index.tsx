4875996ec273584a1f57cf59f843a2300d18eb28
WimWamNerds <chrisisnerdy@gmail.com>
2021-10-02 17:02:18 -0700

    WimWam
    WimWam Network

--- src/index.tsx
+++ src/index.tsx
@@ -1,28 +1,65 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import {App} from './App';
-import reportWebVitals from './reportWebVitals';
-import rootStore from './redux/rootStore'
-import {BrowserRouter} from 'react-router-dom';
-import { Provider } from 'react-redux';
+// Import external components refrence
+import React from 'react'
+import ReactDOM from 'react-dom'
+import injectTapEventPlugin from 'react-tap-event-plugin'
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
+import 'reflect-metadata'
+import 'typeface-roboto'
+import registerServiceWorker from './registerServiceWorker'
+import config from 'src/config'
 
-// export let rerenderEntireTree = () => {
-    ReactDOM.render(
-        <React.StrictMode>
-            <BrowserRouter>
-                <Provider store={rootStore}>
-                <App />
-                </Provider>
-            </BrowserRouter>
-        </React.StrictMode>,
-        document.getElementById('root')
-    );
-// }
+import { Provider } from 'react-redux'
+import configureStore from 'store/configureStore'
+import { ConnectedRouter } from 'connected-react-router/immutable'
 
-// rerenderEntireTree();
+// - Actions
+import * as localeActions from 'store/actions/localeActions'
+import * as globalActions from 'store/actions/globalActions'
 
-// rootStore.subscribe(rerenderEntireTree)
+// - Import app components
+import Master from 'containers/master'
+// import { App } from 'components/AWS'
+// App css
+import './styles/app.css'
 
+/**
+ * Execute startup functions
+ */
+import './socialEngine'
+import rootSaga from 'store/sagas/rootSaga'
 
-reportWebVitals();
+configureStore.runSaga(rootSaga)
+
+// Set default data
+// tslint:disable-next-line:no-empty
+configureStore.store.subscribe(() => { })
+
+// - Initialize languages
+configureStore.store.dispatch(globalActions.initLocale())
+
+// Needed for onClick
+// http://stackoverflow.com/a/34015469/988941
+try { injectTapEventPlugin() } catch (e) { }
+
+const theme = createMuiTheme({
+	palette: {
+		primary: { main: config.theme.primaryColor },
+		secondary: { main: config.theme.secondaryColor },
+	},
+	typography: {
+		useNextVariants: true,
+	},
+})
+
+const supportsHistory = 'pushState' in window.history
+ReactDOM.render(
+	<Provider store={configureStore.store}>
+		<ConnectedRouter history={configureStore.history}>
+			<MuiThemeProvider theme={theme}>
+				<Master />
+			</MuiThemeProvider>
+		</ConnectedRouter>
+	</Provider>,
+	document.getElementById('app') as HTMLElement
+)
+registerServiceWorker()
